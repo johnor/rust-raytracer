909895f2e75c8025f677a04095eb63030680b9ef
Mikael Larsson <mikael.larsson@epiroc.com>
2020-06-03 16:37:00 +0200

    Fix warnings from clippy

--- src/tuple.rs
+++ src/tuple.rs
@@ -8,27 +8,28 @@ pub struct Tuple {
 
 impl Tuple {
     pub fn is_point(&self) -> bool {
-        self.w == 1.0
+        (self.w - 1.0).abs() < std::f64::EPSILON
     }
+
     pub fn is_vector(&self) -> bool {
-        self.w == 0.0
+        self.w.abs() < std::f64::EPSILON
     }
 }
 
 pub fn point(x: f64, y: f64, z: f64) -> Tuple {
     Tuple {
-        x: x,
-        y: y,
-        z: z,
+        x,
+        y,
+        z,
         w: 1.0,
     }
 }
 
 pub fn vector(x: f64, y: f64, z: f64) -> Tuple {
     Tuple {
-        x: x,
-        y: y,
-        z: z,
+        x,
+        y,
+        z,
         w: 0.0,
     }
 }
@@ -38,6 +39,7 @@ mod tests {
     use crate::tuple::point;
     use crate::tuple::vector;
     use crate::tuple::Tuple;
+
     #[test]
     fn tuple_with_w1_is_a_point() {
         let t = Tuple {
@@ -52,6 +54,7 @@ mod tests {
         assert!(t.is_point());
         assert!(!t.is_vector());
     }
+
     #[test]
     fn tuple_with_w0_is_a_vector() {
         let t = Tuple {
@@ -66,6 +69,7 @@ mod tests {
         assert!(!t.is_point());
         assert!(t.is_vector());
     }
+
     #[test]
     fn point_creates_tuples_with_w1() {
         let p = point(4.0, -4.0, 3.0);
@@ -75,8 +79,9 @@ mod tests {
             z: 3.0,
             w: 1.0,
         };
-        assert!(t == p);
+        assert_eq!(t, p);
     }
+
     #[test]
     fn vector_creates_tuples_with_w0() {
         let p = vector(4.0, -4.0, 3.0);
@@ -86,6 +91,6 @@ mod tests {
             z: 3.0,
             w: 0.0,
         };
-        assert!(t == p);
+        assert_eq!(t, p);
     }
 }
