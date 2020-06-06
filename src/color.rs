1a056a27ec4bf2f71c2ff9b1e7ed65a81c9b3524
Johan Norberg <norberg_johan@hotmail.com>
2020-06-07 01:34:13 +0200

    Add new to Color

--- src/color.rs
+++ src/color.rs
@@ -7,6 +7,12 @@ pub struct Color {
     pub b: f64,
 }
 
+impl Color {
+    pub fn new(r: f64, g: f64, b: f64) -> Self {
+        Color { r, g, b }
+    }
+}
+
 impl Add for Color {
     type Output = Self;
 
@@ -73,11 +79,7 @@ mod tests {
 
     #[test]
     fn create_color() {
-        let c = Color {
-            r: -0.5,
-            g: 0.4,
-            b: 1.7,
-        };
+        let c = Color::new(-0.5, 0.4, 1.7);
         assert_eq!(-0.5, c.r);
         assert_eq!(0.4, c.g);
         assert_eq!(1.7, c.b);
@@ -85,84 +87,28 @@ mod tests {
 
     #[test]
     fn color_ops_addition() {
-        let c1 = Color {
-            r: 0.9,
-            g: 0.6,
-            b: 0.75,
-        };
-        let c2 = Color {
-            r: 0.7,
-            g: 0.1,
-            b: 0.25,
-        };
-        assert_color_eq(
-            Color {
-                r: 1.6,
-                g: 0.7,
-                b: 1.0,
-            },
-            c1 + c2,
-        );
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        assert_color_eq(Color::new(1.6, 0.7, 1.0), c1 + c2);
     }
 
     #[test]
     fn color_ops_subtraction() {
-        let c1 = Color {
-            r: 0.9,
-            g: 0.6,
-            b: 0.75,
-        };
-        let c2 = Color {
-            r: 0.7,
-            g: 0.1,
-            b: 0.25,
-        };
-        assert_color_eq(
-            Color {
-                r: 0.2,
-                g: 0.5,
-                b: 0.5,
-            },
-            c1 - c2,
-        );
+        let c1 = Color::new(0.9, 0.6, 0.75);
+        let c2 = Color::new(0.7, 0.1, 0.25);
+        assert_color_eq(Color::new(0.2, 0.5, 0.5), c1 - c2);
     }
 
     #[test]
     fn color_ops_multiply_colors() {
-        let c1 = Color {
-            r: 1.0,
-            g: 0.2,
-            b: 0.4,
-        };
-        let c2 = Color {
-            r: 0.9,
-            g: 1.0,
-            b: 0.1,
-        };
-        assert_color_eq(
-            Color {
-                r: 0.9,
-                g: 0.2,
-                b: 0.04,
-            },
-            c1 * c2,
-        );
+        let c1 = Color::new(1.0, 0.2, 0.4);
+        let c2 = Color::new(0.9, 1.0, 0.1);
+        assert_color_eq(Color::new(0.9, 0.2, 0.04), c1 * c2);
     }
 
     #[test]
     fn color_ops_multiply_by_scalar() {
-        let c = Color {
-            r: 0.2,
-            g: 0.3,
-            b: 0.4,
-        };
-        assert_color_eq(
-            Color {
-                r: 0.4,
-                g: 0.6,
-                b: 0.8,
-            },
-            c * 2.0,
-        );
+        let c = Color::new(0.2, 0.3, 0.4);
+        assert_color_eq(Color::new(0.4, 0.6, 0.8), c * 2.0);
     }
 }
