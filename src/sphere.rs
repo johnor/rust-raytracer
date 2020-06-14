114bc4941095c78307f9390efdfac66f9dab71a2
Johan Norberg <norberg_johan@hotmail.com>
2020-06-15 00:17:01 +0200

    Add transform to sphere

--- src/sphere.rs
+++ src/sphere.rs
@@ -1,23 +1,27 @@
 use crate::intersections::Intersection;
+use crate::matrix::Mat4x4;
 use crate::ray::Ray;
 use crate::tuple::{point, Tuple};
 
 #[derive(Debug, PartialEq)]
 pub struct Sphere {
     pub origin: Tuple,
+    pub transform: Mat4x4,
 }
 
 impl Sphere {
     pub fn new() -> Self {
         Sphere {
             origin: point(0.0, 0.0, 0.0),
+            transform: Mat4x4::identity(),
         }
     }
 
     pub fn intersect(&self, ray: Ray) -> Vec<Intersection> {
-        let sphere_to_ray = ray.origin - self.origin;
-        let a = ray.direction.dot(ray.direction);
-        let b = 2. * ray.direction.dot(sphere_to_ray);
+        let ray2 = self.transform.inverse().unwrap() * ray;
+        let sphere_to_ray = ray2.origin - self.origin;
+        let a = ray2.direction.dot(ray2.direction);
+        let b = 2. * ray2.direction.dot(sphere_to_ray);
         let c = sphere_to_ray.dot(sphere_to_ray) - 1.0;
 
         let discriminant = b * b - 4.0 * a * c;
@@ -34,10 +38,11 @@ impl Sphere {
 
 #[cfg(test)]
 mod tests {
+    use crate::matrix::Mat4x4;
     use crate::ray::Ray;
     use crate::sphere::Sphere;
-    use crate::tuple::point;
-    use crate::tuple::vector;
+    use crate::transform;
+    use crate::tuple::{point, vector};
 
     #[test]
     fn ray_and_sphere_intersects_at_two_points() {
@@ -100,5 +105,41 @@ mod tests {
         let xs = s.intersect(r);
         assert_eq!(&s, xs[0].object);
         assert_eq!(&s, xs[1].object);
+    }
+
+    #[test]
+    fn sphere_default_transformation() {
+        let s = Sphere::new();
+        assert_eq!(Mat4x4::identity(), s.transform);
+    }
+
+    #[test]
+    fn sphere_change_transformation() {
+        let mut s = Sphere::new();
+        let t = transform::translate(2.0, 3.0, 4.0);
+        s.transform = t;
+        assert_eq!(t, s.transform);
+    }
+
+    #[test]
+    fn intersect_scaled_sphere_with_ray() {
+        let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+        let mut s = Sphere::new();
+        s.transform = transform::scale(2.0, 2.0, 2.0);
+
+        let xs = s.intersect(r);
+        assert_eq!(2, xs.len());
+        assert_eq!(3.0, xs[0].t);
+        assert_eq!(7.0, xs[1].t);
+    }
+
+    #[test]
+    fn intersect_translated_sphere_with_ray() {
+        let r = Ray::new(point(0.0, 0.0, -5.0), vector(0.0, 0.0, 1.0));
+        let mut s = Sphere::new();
+        s.transform = transform::translate(5.0, 0.0, 0.0);
+
+        let xs = s.intersect(r);
+        assert_eq!(0, xs.len());
     }
 }
