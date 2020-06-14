619e252196c6701538931f74cecf69b1c3ff8af7
Johan Norberg <norberg_johan@hotmail.com>
2020-06-15 00:06:53 +0200

    Add support to transform a ray

--- src/ray.rs
+++ src/ray.rs
@@ -1,4 +1,5 @@
-use crate::tuple;
+use crate::{matrix, tuple};
+use std::ops::Mul;
 
 #[derive(Debug)]
 pub struct Ray {
@@ -16,9 +17,18 @@ impl Ray {
     }
 }
 
+impl Mul<Ray> for matrix::Mat4x4 {
+    type Output = Ray;
+
+    fn mul(self, rhs: Ray) -> Ray {
+        Ray::new(self * rhs.origin, self * rhs.direction)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::ray::Ray;
+    use crate::transform;
     use crate::tuple::{point, vector};
 
     #[test]
@@ -39,5 +49,23 @@ mod tests {
         assert_eq!(point(3.0, 3.0, 4.0), r.position(1.0));
         assert_eq!(point(1.0, 3.0, 4.0), r.position(-1.0));
         assert_eq!(point(4.5, 3.0, 4.0), r.position(2.5));
+    }
+
+    #[test]
+    fn translating_a_ray() {
+        let r = Ray::new(point(1.0, 2.0, 3.0), vector(0.0, 1.0, 0.0));
+        let m = transform::translate(3.0, 4.0, 5.0);
+        let r2 = m * r;
+        assert_eq!(point(4.0, 6.0, 8.0), r2.origin);
+        assert_eq!(vector(0.0, 1.0, 0.0), r2.direction);
+    }
+
+    #[test]
+    fn scaling_a_ray() {
+        let r = Ray::new(point(1.0, 2.0, 3.0), vector(0.0, 1.0, 0.0));
+        let m = transform::scale(2.0, 3.0, 4.0);
+        let r2 = m * r;
+        assert_eq!(point(2.0, 6.0, 12.0), r2.origin);
+        assert_eq!(vector(0.0, 3.0, 0.0), r2.direction);
     }
 }
