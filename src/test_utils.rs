c8b3e54520358e9fbfbc0be3af9db5f96f4c63f9
Johan Norberg <norberg_johan@hotmail.com>
2020-06-25 03:24:20 +0200

    Implement ray_for_pixel in Camera

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,4 +1,15 @@
 #[cfg(test)]
+use crate::tuple::Tuple;
+
+#[cfg(test)]
 pub fn assert_near(v1: f64, v2: f64) {
     assert!((v1 - v2).abs() < std::f64::EPSILON);
+}
+
+#[cfg(test)]
+pub fn assert_tuple_near(t1: Tuple, t2: Tuple, tol: f64) {
+    assert!((t1.x - t2.x).abs() < tol);
+    assert!((t1.y - t2.y).abs() < tol);
+    assert!((t1.z - t2.z).abs() < tol);
+    assert!((t1.w - t2.w).abs() < tol);
 }
